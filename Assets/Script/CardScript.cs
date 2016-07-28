59e8fef07ffa24a22414c0f0a79eec0974e68bfc
kawamurashin <kawamura.shin@gmail.com>
2016-07-28 18:44:54 +0900

    first commit

---
+++ Assets/Script/CardScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardScript : MonoBehaviour {
+    SpriteRenderer spriteRender;
+    public void setTexture(Texture2D texture)
+    {
+        Sprite sp = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0, 0));
+        spriteRender.sprite = sp;
+    }
+    void Awake()
+    {
+       Transform transform = this.transform.FindChild("New Sprite");
+       spriteRender = (SpriteRenderer)transform.GetComponent("SpriteRenderer");
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 vector = this.transform.position;
+        vector.x += 0.1f - 0.2f * Random.value;
+        vector.y += 0.1f - 0.2f * Random.value;
+        this.transform.position = vector;
+
+    }
+}
---
+++ Assets/Script/Main.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Main : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        string url = "file://" + Application.dataPath + "/Data/image/red.png";
+        StartCoroutine(GetWww(url));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    /**/
+    IEnumerator GetWww(string url)
+    {
+        WWW www = new WWW(url);
+        yield return www;
+
+        Texture2D textrure = www.texture;
+
+        GameObject gameObject = Instantiate(Resources.Load("Card")) as GameObject;
+        CardScript cardScrpt = gameObject.GetComponent<CardScript>();
+        cardScrpt.setTexture(textrure);
+
+    }
+    
+}
